24082baf1572f8fa6085dc2641fe70fc13c74308
RakhmonaliAnaqulov <rakhmonaliandaqulov@gmail.com>
2023-04-27 18:07:33 +0500

    attach fixed and finished

--- src/main/java/com/example/controller/AttachController.java
+++ src/main/java/com/example/controller/AttachController.java
@@ -1,8 +1,10 @@
 package com.example.controller;
 
+import com.example.dto.attach.AttachDto;
 import com.example.service.AttachService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,32 +17,17 @@ public class AttachController {
     @Autowired
     private AttachService attachService;
 
-     @GetMapping(value = "/open_general/{fileName}", produces = MediaType.ALL_VALUE)
-    public byte[] open_general(@PathVariable("fileName") String fileName) {
-        return attachService.open_general(fileName);
-    }
-
-    @PostMapping("/upload/1")
+    @PostMapping("/upload")
     public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file) {
-        String fileName = attachService.saveToSystem(file);
-        return ResponseEntity.ok().body(fileName);
-    }
-    @PostMapping("/upload/2")
-    public ResponseEntity<String> upload2(@RequestParam("file") MultipartFile file) {
         String fileName = attachService.saveToSystem2(file);
         return ResponseEntity.ok().body(fileName);
     }
-    @PostMapping("/upload/3")
-    public ResponseEntity<String> upload3(@RequestParam("file") MultipartFile file) {
-        String fileName = attachService.saveToSystem3(file);
-        return ResponseEntity.ok().body(fileName);
-    }
 
-    @GetMapping(value = "/open/{id}", produces = MediaType.IMAGE_PNG_VALUE)
-    public byte[] open(@PathVariable("id") String id) {
-        if (id != null && id.length() > 0) {
+    @GetMapping(value = "/open/{fileName}", produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] open(@PathVariable("fileName") String fileName) {
+        if (fileName != null && fileName.length() > 0) {
             try {
-                return this.attachService.loadImage(id);
+                return this.attachService.loadImage(fileName);
             } catch (Exception e) {
                 e.printStackTrace();
                 return new byte[0];
@@ -48,14 +35,16 @@ public class AttachController {
         }
         return null;
     }
-    @GetMapping(value = "/open_general/v2/{id}", produces = MediaType.ALL_VALUE)
-    public byte[] open_general2(@PathVariable("id") String id) {
-        return attachService.open_general2(id);
+
+
+    @GetMapping(value = "/open_general/{fileName}", produces = MediaType.ALL_VALUE)
+    public byte[] open_general(@PathVariable("fileName") String fileName) {
+        return attachService.open_general2(fileName);
     }
 
     @GetMapping("/download/{fineName}")
-    public ResponseEntity<Resource> download(@PathVariable("fileName") String fileName) {
-        Resource file = attachService.download(fileName);
+    public ResponseEntity<Resource> download(@PathVariable("fineName") String fileName) {
+        Resource file = (Resource) attachService.download(fileName);
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
                 "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
@@ -63,6 +52,13 @@ public class AttachController {
     @DeleteMapping("/deleteById/{id}")
     public ResponseEntity<Boolean> delete(@PathVariable("id") String id){
         return ResponseEntity.ok(attachService.delete(id));
+    }
+
+    @GetMapping("/list-paging")
+    public ResponseEntity<Page<AttachDto>> getAllWithPagination(
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "size", defaultValue = "3") int size) {
+        return ResponseEntity.ok(attachService.getAll(page, size));
     }
 
 
--- src/main/java/com/example/service/AttachService.java
+++ src/main/java/com/example/service/AttachService.java
@@ -1,5 +1,6 @@
 package com.example.service;
 
+import com.example.dto.attach.AttachDto;
 import com.example.entity.ArticleEntity;
 import com.example.entity.AttachEntity;
 import com.example.exps.ItemNotFoundException;
@@ -7,6 +8,7 @@ import com.example.repository.AttachRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,9 +22,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
-import java.util.Calendar;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 
 @Service
@@ -31,21 +31,23 @@ public class AttachService {
     private AttachRepository attachRepository;
 
     public String saveToSystem(MultipartFile file) {
-            try {
-                File folder = new File("attaches");
-                if (!folder.exists()) {
-                    folder.mkdir();
-                }
-                byte[] bytes = file.getBytes();
-                Path path = Paths.get("attaches/" + file.getOriginalFilename());
-                // attaches/test_imge_1.jpg
-                Files.write(path, bytes);
-                return file.getOriginalFilename();
-            } catch (IOException e) {
-                e.printStackTrace();
+        try {
+            File folder = new File("attaches");
+            if (!folder.exists()) {
+                folder.mkdir();
             }
-            return null;
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get("attaches/" + file.getOriginalFilename());
+            // attaches/test_imge_1.jpg
+            // attaches/uuid().jpg
+            Files.write(path, bytes);
+            return file.getOriginalFilename();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
+
     public String saveToSystem2(MultipartFile file) {
         try {
             File folder = new File("attaches");
@@ -53,67 +55,27 @@ public class AttachService {
                 folder.mkdir();
             }
             byte[] bytes = file.getBytes();
-            String extension = getExtension(file.getOriginalFilename());
-            String fileName = UUID.randomUUID().toString();
-            Path path = Paths.get("attaches/" + fileName + "." + extension);
+            String extension = file.getOriginalFilename();
+            Path path = Paths.get("attaches/" + file.getOriginalFilename());
             // attaches/test_imge_1.jpg
             // attaches/uuid().jpg
             Files.write(path, bytes);
-            return fileName + "." + extension;
+
+            AttachEntity attachEntity = new AttachEntity();
+            attachEntity.setId(UUID.randomUUID().toString());
+            attachEntity.setSize(file.getSize());
+            attachEntity.setPath(path.toString());
+            attachEntity.setOriginalName(file.getOriginalFilename());
+            attachEntity.setCreatedData(LocalDateTime.now());
+            attachEntity.setExtension(extension);
+            attachRepository.save(attachEntity);
+            return file.getOriginalFilename();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    public String getExtension(String fileName) { // mp3/jpg/npg/mp4.....
-        int lastIndex = fileName.lastIndexOf(".");
-        return fileName.substring(lastIndex + 1);
-    }
-
-    public byte[] loadImage(String fileName) {
-        byte[] imageInByte;
-
-        BufferedImage originalImage;
-        try {
-            originalImage = ImageIO.read(new File("attaches/" + fileName));
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(originalImage, "png", baos);
-            baos.flush();
-            imageInByte = baos.toByteArray();
-            baos.close();
-            return imageInByte;
-        } catch (Exception e) {
-            return new byte[0];
-        }
-    }
-    public byte[] open_general(String attachName) {
-        byte[] data;
-        try {
-            Path file = Paths.get("attaches/" + attachName);
-            data = Files.readAllBytes(file);
-            return data;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return new byte[0];
-    }
-
-    public byte[] open_general2(String attachName) {
-        // 20f0f915-93ec-4099-97e3-c1cb7a95151f.jpg
-        int lastIndex = attachName.lastIndexOf(".");
-        String id = attachName.substring(0,lastIndex);
-        AttachEntity attachEntity = get(id);
-        byte[] data;
-        try {                                                     // attaches/2023/4/25/20f0f915-93ec-4099-97e3-c1cb7a95151f.jpg
-            Path file = Paths.get("attaches/" + attachEntity.getPath() + "/" + attachName);
-            data = Files.readAllBytes(file);
-            return data;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return new byte[0];
-    }
     public String saveToSystem3(MultipartFile file) {
         try {
             String pathFolder = getYmDString(); // 2022/04/23
@@ -143,6 +105,57 @@ public class AttachService {
         return null;
     }
 
+
+    public byte[] loadImage(String fileName) {
+        byte[] imageInByte;
+
+        BufferedImage originalImage;
+        try {
+            originalImage = ImageIO.read(new File("attaches/" + fileName));
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ImageIO.write(originalImage, "png", baos);
+            baos.flush();
+            imageInByte = baos.toByteArray();
+            baos.close();
+            return imageInByte;
+        } catch (Exception e) {
+            return new byte[0];
+        }
+    }
+
+    public byte[] open_general(String attachName) {
+        byte[] data;
+        try {
+            Path file = Paths.get("attaches/" + attachName);
+            data = Files.readAllBytes(file);
+            return data;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new byte[0];
+    }
+
+    public byte[] open_general2(String attachName) {
+        // 20f0f915-93ec-4099-97e3-c1cb7a95151f.jpg
+        int lastIndex = attachName.lastIndexOf(".");
+        String id = attachName.substring(0, lastIndex);
+        AttachEntity attachEntity = get(id);
+        byte[] data;
+        try {                                                     // attaches/2023/4/25/20f0f915-93ec-4099-97e3-c1cb7a95151f.jpg
+            Path file = Paths.get("attaches/" + attachEntity.getPath() + "/" + attachName);
+            data = Files.readAllBytes(file);
+            return data;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new byte[0];
+    }
+
+    public String getExtension(String fileName) { // mp3/jpg/npg/mp4.....
+        int lastIndex = fileName.lastIndexOf(".");
+        return fileName.substring(lastIndex + 1);
+    }
+
     public String getYmDString() {
         int year = Calendar.getInstance().get(Calendar.YEAR);
         int month = Calendar.getInstance().get(Calendar.MONTH) + 1;
@@ -152,15 +165,22 @@ public class AttachService {
     }
 
     public AttachEntity get(String id) {
-        return attachRepository.findById(String.valueOf(id)).orElseThrow(() -> {
-            throw new ItemNotFoundException("Attach not ound");
+        System.out.println("sout>>"+id+"<<");
+        return attachRepository.findById(id).orElseThrow(() -> {
+            throw new ItemNotFoundException("Attach not found");
         });
     }
 
+
     public Resource download(String fileName) {
         try {
+            int lastIndex = fileName.lastIndexOf(".");
+            String id = fileName.substring(0, lastIndex);
+            AttachEntity attachEntity = get(id);
+
             Path file = Paths.get("attaches/" + fileName);
-            Resource resource = new UrlResource(file.toUri());
+            System.out.println("attaches/" + fileName);
+            org.springframework.core.io.Resource resource = new UrlResource(file.toUri());
             if (resource.exists() || resource.isReadable()) {
                 return resource;
             } else {
@@ -179,4 +199,29 @@ public class AttachService {
         return true;
     }
 
+    public Page<AttachDto> getAll(int page, int size) {
+        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+        Pageable paging = PageRequest.of(page - 1, size, sort);
+        Page<AttachEntity> pageObj = attachRepository.findAll(paging);
+
+        Long totalCount = pageObj.getTotalElements();
+
+        List<AttachEntity> entityList = pageObj.getContent();
+        List<AttachDto> dtoList = new LinkedList<>();
+        if (pageObj != null) {
+            for (AttachEntity entity : entityList) {
+                AttachDto dto = new AttachDto();
+                dto.setId(entity.getId());
+                dto.setSize(entity.getSize());
+                dto.setPath(entity.getPath());
+                dto.setExtension(entity.getExtension());
+                dto.setCreatedData(entity.getCreatedData());
+                dto.setOriginalName(entity.getOriginalName());
+                dtoList.add(dto);
+            }
+            Page<AttachDto> response = new PageImpl<AttachDto>(dtoList, paging, totalCount);
+            return response;
+        }
+        throw new ItemNotFoundException("List id empty");
+    }
 }
