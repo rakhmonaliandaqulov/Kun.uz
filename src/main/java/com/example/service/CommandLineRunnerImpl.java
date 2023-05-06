e5edeccaa7396a2d2a79345c4a4fe713c8b222d9
RakhmonaliAnaqulov <rakhmonaliandaqulov@gmail.com>
2023-05-06 19:57:08 +0500

    trigger almost finished

--- src/main/java/com/example/service/CommandLineRunnerImpl.java
+++ src/main/java/com/example/service/CommandLineRunnerImpl.java
@@ -5,20 +5,24 @@ import com.example.enums.GeneralStatus;
 import com.example.enums.ProfileRole;
 import com.example.repository.ProfileRepository;
 import com.example.util.MD5Util;
+import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.sql.DataSource;
 import java.util.Optional;
 
 @Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
     @Autowired
     private ProfileRepository profileRepository;
+    @Autowired
+    private DataSource dataSource;
     @Override
     public void run(String... args) {
-        System.out.println("App started");
+        /*System.out.println("App started");
         String email = "adminjon_mazgi@gmail.com";
         Optional<ProfileEntity> profileEntity = profileRepository.findByEmail(email);
         if (profileEntity.isEmpty()) {
@@ -32,7 +36,7 @@ public class CommandLineRunnerImpl implements CommandLineRunner {
             entity.setStatus(GeneralStatus.ACTIVE);
             profileRepository.save(entity);
             System.out.println("Admin created");
-        }
-
+        }*/
+        Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
     }
 }
